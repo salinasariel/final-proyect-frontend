6b6c8e49aa0e940d42a0625e1a7cb65e37625845
Julian Vazquez <julianvazquez12@gmail.com>
2024-04-17 02:48:01 -0300

    ChatRobot v1.0

--- pps_front/src/App.jsx
+++ pps_front/src/App.jsx
@@ -1,11 +1,17 @@
-
+import ChatBot from 'react-simple-chatbot'
 import './App.css'
 import AppRouter from '/src/router/AppRouter.jsx';
+import chatbotSteps from './ChatBot/ChatBotConfig';
 
 function App() {
+
   return (
-      
+      <>
           <AppRouter/>
+          
+        <ChatBot steps={chatbotSteps} headerTitle="Robotito" />
+
+      </>
   )
 }
 
