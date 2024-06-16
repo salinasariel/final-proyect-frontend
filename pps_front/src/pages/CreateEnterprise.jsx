d20c4ab63e63c414891a6e42daf6e86695ca9c9d
Ariel <93411684+salinasariel@users.noreply.github.com>
2024-06-16 15:17:38 -0300

    create enterprise

--- pps_front/src/pages/CreateEnterprise.jsx
+++ pps_front/src/pages/CreateEnterprise.jsx
@@ -1,102 +1,115 @@
+import React, { useState } from 'react';
+import { TextField, Button, Typography } from "@mui/material";
+import Header from '../components/Header';
 import Footer from "../components/Footer";
+import api from '../api';
+
+import { toast } from "react-toastify";
 
 const CreateEnterprise = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [city, setCity] = useState('');
+  const [password, setPassword] = useState('');
+  const [cuitnumber, setcuitNumber] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const enterpriseData = {
+
+      email: email,
+      password: password,
+      cuit: cuitnumber,
+      name: name,
+      city: city,
+      rol: 0,
+      userState: true
+
+    };
+
+    try {
+      const response = await api.post("/User/CreateEnterprise", enterpriseData);
+      toast.success(`Estudiante creado correctamente con ID: ${response.data}`);
+    } catch (error) {
+      console.error("Error creando estudiante:", error);
+      toast.error(`Error creando estudiante: ${error.message}`);
+    }
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white shadow-md rounded-lg p-8 max-w-md">
-        <h1 className="text-3xl font-bold text-center mb-4">
-          Solicitud de Alta Empresas
-        </h1>
-        <form className="flex flex-col gap-4">
-          <div className="flex flex-col">
-            <label htmlFor="legajo" className="font-semibold">
-              CUIT
-            </label>
-            <input
-              id="legajo"
-              type="text"
-              placeholder="Ingrese el CUIT de la empresa"
-              className="rounded-md px-4 py-2 border border-gray-300 focus:outline-none focus:border-blue-500"
+    <>
+      <Header />
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className='max-w-[50%]'>
+          <form onSubmit={handleSubmit}>
+            <Typography variant="h6" style={{ paddingTop: "5px" }}>
+              <b>Solicitud de alta empresas</b>
+            </Typography>
+            <TextField
+              id="fileNumber"
+              margin="normal"
+              label="CUIT"
+              required
+              fullWidth
+              variant="outlined"
+              color="secondary"
+              value={cuitnumber}
+              onChange={(e) => setcuitNumber(e.target.value)}
             />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="nombre" className="font-semibold">
-              Nombre
-            </label>
-            <input
-              id="nombre"
-              type="text"
-              placeholder="Ingrese el nombre de la empresa"
-              className="rounded-md px-4 py-2 border border-gray-300 focus:outline-none focus:border-blue-500"
+            <TextField
+              id="name"
+              margin="normal"
+              label="Nombre Institucional"
+              required
+              fullWidth
+              variant="outlined"
+              color="secondary"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="apellido" className="font-semibold">
-              Localizacion
-            </label>
-            <input
-              id="apellido"
-              type="text"
-              placeholder="¿Donde se encuentra la empresa?"
-              className="rounded-md px-4 py-2 border border-gray-300 focus:outline-none focus:border-blue-500"
+            <TextField
+              id="city"
+              margin="normal"
+              label="Ciudad"
+              required
+              fullWidth
+              variant="outlined"
+              color="secondary"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
             />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="sexo" className="font-semibold">
-              Empleados
-            </label>
-            <input
-            type="number"
-              id="employees"
-              
-              placeholder="¿Cuantos empleados tiene la empresa?"
-              className="rounded-md px-4 py-2 border border-gray-300 focus:outline-none focus:border-blue-500"
+            <TextField
+              id='email'
+              variant="outlined"
+              label="Email de contacto"
+              fullWidth
+              required
+              margin="normal"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="ciudad" className="font-semibold">
-              Sector
-            </label>
-            <input
-              id="ciudad"
-              type="text"
-              placeholder="¿Cual es el sector de la empresa?"
-              className="rounded-md px-4 py-2 border border-gray-300 focus:outline-none focus:border-blue-500"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="telefono" className="font-semibold">
-              Teléfono
-            </label>
-            <input
-              id="telefono"
-              type="number"
-              placeholder="Ingrese su número de teléfono"
-              className="rounded-md px-4 py-2 border border-gray-300 focus:outline-none focus:border-blue-500"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="clave" className="font-semibold">
-              Clave
-            </label>
-            <input
-              id="clave"
+            <TextField
+              id="password"
+              margin="normal"
+              label="Contraseña"
               type="password"
-              placeholder="Ingrese su clave alfanumérica"
-              className="rounded-md px-4 py-2 border border-gray-300 focus:outline-none focus:border-blue-500"
+              required
+              fullWidth
+              variant="outlined"
+              color="secondary"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
-          </div>
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 rounded-md transition duration-300"
-          >
-            Enviar Solicitud
-          </button>
-        </form>
-        
+
+            <Button type="submit" variant="contained" color="primary">
+              Crear Estudiante
+            </Button>
+          </form>
+        </div>
       </div>
-      <Footer />
-    </div>
+      <Footer youarenterprise={false} />
+
+    </>
   );
 };
 
