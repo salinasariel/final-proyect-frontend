13938a95b4424388c163e9ba395decd24dee35bc
Ariel <93411684+salinasariel@users.noreply.github.com>
2024-06-28 02:27:21 -0300

    fix bugs

--- pps_front/src/components/SendOffer.jsx
+++ pps_front/src/components/SendOffer.jsx
@@ -34,7 +34,7 @@ const SendOffer = () => {
             console.error("tokenData o tokenData.userid es nulo.");
           }
         } catch (error) {
-            toast.error(`No pudimos enviar tu postulación.`);
+            toast.error(`No es posible realizar la postulacion.`);
         }
       };
 
